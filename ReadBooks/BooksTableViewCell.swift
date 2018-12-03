34a6401474c6fd2f4de9b1bd09a83655d37174af
Andrey Petrovskiy <andreypetrovskiy@MacBook-Pro-Andrey.local>
2018-12-03 23:49:31 +0200

    first commit

---
+++ ReadBooks/BooksTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BooksTableViewCell.swift
+//  ReadBooks
+//
+//  Created by Andrey Petrovskiy on 03.12.2018.
+//  Copyright © 2018 Andrey Petrovskiy. All rights reserved.
+//
+
+import UIKit
+
+class BooksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
