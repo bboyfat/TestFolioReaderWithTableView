5c8dbda680351331fbd6e6ce4807cce63af97512
Andrey Petrovskiy <andreypetrovskiy@MacBook-Pro-Andrey.local>
2018-12-04 00:27:54 +0200

    add

--- ReadBooks/TableViewController.swift
+++ ReadBooks/TableViewController.swift
@@ -29,7 +29,7 @@ class TableViewController: UITableViewController {
        
 
         // Uncomment the following line to preserve selection between presentations
-         self.clearsSelectionOnViewWillAppear = false
+         self.clearsSelectionOnViewWillAppear = true
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
